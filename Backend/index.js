b32ca6a329d47026d03342b3902cbff7eea4a31c
A-Patel033 <72783255+A-Patel033@users.noreply.github.com>
2021-08-01 11:26:14 +0930

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 app.use('/users', route);
 
-const URL = 'mongodb+srv://Abhi:Abhi7013@crud-with-mern-stack.wrggx.mongodb.net/CRUD-With-MERN-Stack?retryWrites=true&w=majority';
+const URL = 'mongodb+srv://Abhi:******@crud-with-mern-stack.wrggx.mongodb.net/CRUD-With-MERN-Stack?retryWrites=true&w=majority';
 mongoose.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true}).then(() => {
     app.listen(PORT, () =>{
         console.log(`Server is running on ${PORT}`);
